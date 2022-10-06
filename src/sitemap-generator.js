1d9e932e3f5fe13a473bde11d956034eefaab55b
Arvind Waghmare <Arvind.Waghmare@morningstar.com>
2022-10-06 08:49:38 +0530

    adding sitemap per file options

--- src/sitemap-generator.js
+++ src/sitemap-generator.js
@@ -1,28 +1,27 @@
-require("babel-register")({
-    presets: ["es2015", "react"]
-  });
-   
-  const router = require("./sitemap-routes").default;
-  const Sitemap = require("react-router-sitemap").default;
-  
-  async function generateSitemap() {
-    try {
+import router from "./sitemap-routes";
+import Sitemap, { pathsSplitter } from "react-router-sitemap";
 
-    const idMap = [{id: "0001"}, {id: "0002"}, {id: "0003"}, {id: "0004"}, {id: "0005"}]
-  
-      const paramsConfig = {
-        "/page/:id": idMap
-      };
-  
-      return (
-        new Sitemap(router)
-            .applyParams(paramsConfig)
-            .build("https://www.example.com")
-            .save("./public/sitemap-react.xml")
-      );
-    } catch(e) {
-      console.log(e);
-    } 
+async function generateSitemap() {
+  try {
+    const idMap = [
+      { id: "0001" },
+      { id: "0002" },
+      { id: "0003" },
+      { id: "0004" },
+      { id: "0005" },
+    ];
+
+    const paramsConfig = {
+      "/page/:id": idMap,
+    };
+
+    return new Sitemap(router)
+      .applyParams(paramsConfig)
+      .build("https://localhost:3000", {limitCountPaths:3})
+      .save("./public/sitemap-react.xml");
+  } catch (e) {
+    console.log(e);
   }
-  
-  generateSitemap();
+}
+
+generateSitemap();
--- src/sitemap-routes.js
+++ src/sitemap-routes.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { Route } from "react-router";
+import { Switch, Route } from "react-router";
 
 export default (
-  <Route>
+  // Switch is added in v4 react-router
+  <Switch>
     <Route path="/" />
+    <Route path="/about" />
+    <Route path="/projects" />
+    <Route path="/contacts" />
+    <Route path="/auth" />
     <Route path="/page/:id" />
-  </Route>
+    <Route />
+  </Switch>
 );
